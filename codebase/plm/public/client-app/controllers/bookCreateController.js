864008c04423df67cc91df132da5ec37a65c4877
Anindita Sarkar <s.anindita16@gmail.com>
2016-05-25 14:38:07 +0530

    Client side validation implemented for books.

--- codebase/plm/public/client-app/controllers/bookCreateController.js
+++ codebase/plm/public/client-app/controllers/bookCreateController.js
@@ -1,10 +1,11 @@
 app.controller('BookCreateController', function ($scope, $state, BookRespository, GenreRespository) {
     $scope.book = {};
     $scope.genres = GenreRespository.query();
-    $scope.addBook = function () {
-        BookRespository.save($scope.book, function () {
-            $state.go('books');
-        });
-
-    }
+    $scope.addBook = function (isValid) {
+        if (isValid) {
+            BookRespository.save($scope.book, function () {
+                $state.go('books');
+            });
+        }
+   }
 });
